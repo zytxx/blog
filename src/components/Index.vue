864b6049c6f430f8d3409f2299954df6e8a2af2d
zytxx <854148939@qq.com>
2018-06-26 01:15:22 +0800

    blog 搭建

---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div><span>{{aa}}</span></div>
+</template>
+
+<style lang="scss"  scoped>
+  div{
+    background: lightblue;
+    width: 100px;
+    height: 100px;
+    span{
+      font-size: 12px;
+      color:red;
+    }
+  }
+</style>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      aa: '我是主页'
+    }
+  }
+}
+</script>
